e78a992b5113535b86510480ecc9d1a1d58c9c5a
MaciejLesniak97 <macieklesniak7@wp.pl>
2020-05-24 23:37:48 +0200

    third commit - roomCount

---
+++ chat.js
@@ -0,0 +1,80 @@
+import { isWebSocketCloseEvent } from "https://deno.land/std/ws/mod.ts";
+import { v4 } from "https://deno.land/std/uuid/mod.ts";
+
+
+/**
+ * userId: {
+ *      userId: string,
+ *      name: string,
+ *      groupName: string,
+ *      ws: WebSocket
+ * }
+ */
+const usersMap = new Map();
+
+/**
+ * groupName: [user1, user2]
+ * 
+ * {
+ *      userId: string,
+ *      name: string,
+ *      groupName: string,
+ *      ws: WebSocket
+ * }
+ */
+const groupsMap = new Map();
+
+export default async function chat(ws) {
+    console.log(`connected`);
+    const userId = v4.generate();
+
+    for await (let data of ws) {
+        const event = typeof data === 'string' ? JSON.parse(data) : data;
+
+        if (isWebSocketCloseEvent(data)) {
+            
+            const userObj = usersMap.get(userId);
+            const users = groupsMap.get(userObj.groupName) || [];
+            users = users.filter(u => u.userId !== userId);
+            groupsMap.set(userObj.groupName, users);
+            usersMap.delete(userId);
+            
+            emitEvent(userObj.groupName);
+            break;
+        }
+
+        switch (event.event) {
+            case 'join':
+                const userObj = {
+                    userId,
+                    name: event.name,
+                    groupName: event.groupName,
+                    ws
+                };
+                usersMap.set(userId, userObj);
+                const users = groupsMap.get(event.groupName) || [];
+                users.push(userObj);
+                groupsMap.set(event.groupName, users);
+
+                emitEvent(event.groupName);
+        }
+    }
+}
+
+function emitEvent(groupName) {
+    const users = groupsMap.get(groupName) || [];
+    for (const user of users) {
+        const event = {
+            event: 'users',
+            data: getDisplayUsers(groupName)
+        }
+        user.ws.send(JSON.stringify(event))
+    }
+}
+
+function getDisplayUsers(groupName){
+    const users = groupsMap.get(groupName) || [];
+    return users.map(u => {
+        return {userId: u.userId, name: u.name};
+    })
+}
--- public/client.js
+++ public/client.js
@@ -1,15 +1,54 @@
+
 let ws;
+let chatUsersCtr = document.querySelector('#chatUsers');
+let chatUsersCount = document.querySelector('#chatUsersCount');
 
 window.addEventListener('DOMContentLoaded', () => {
     ws = new WebSocket(`ws://localhost:3000/ws`);
     ws.addEventListener('open', onConnectionOpen);
-    ws.addEventListener('open', onMessageReceived);
+    ws.addEventListener('message', onMessageReceived);
 })
 
 function onConnectionOpen() {
     console.log(`Connection Opened`);
+    const queryParams = getQueryParams();
+    if (!queryParams.name || !queryParams.group) {
+        window.location.href = 'chat.html';
+        return;
+    }
+    const event = {
+        event: 'join',
+        groupName: queryParams.group,
+        name: queryParams.name
+    }
+    ws.send(JSON.stringify(event));
 }
 
 function onMessageReceived(event){
-    console.log("Message received", event);
+    console.log("Message received");
+    event = JSON.parse(event.data);
+    console.log(data);
+    switch(event.event) {
+        case 'users':
+            chatUsersCount.innerHTML = event.data.length;
+            chatUsersCtr.innerHTML = '';
+            event.data.forEach(u => {
+                const userEl = document.createElement('div');
+                userEl.className = 'chat-user';
+                userEl.innerHTML = u.name;
+                chatUsersCtr.appendChild(userEl)
+            })
+    }
+}
+
+function getQueryParams() {
+    const search = window.location.search.substring(1);
+    const pairs = search.split("&");
+    const params = {};
+    for (const pair of pairs) {
+        const parts = pair.split("=");
+        params[decodeURIComponent(parts[0])] = decodeURIComponent(parts[1]);
+    }
+
+    return; 
 }
